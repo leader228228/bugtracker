f6789e34051cf263089b4c9349666db5670f5614
leader228228 <44357863+leader228228@users.noreply.github.com>
2019-05-04 17:40:06 +0300

    [~] - RequestValidator changed (map of schemas -> superschema)

--- module_Main/src/main/java/json/RequestValidatorImpl.java
+++ module_Main/src/main/java/json/RequestValidatorImpl.java
@@ -1,37 +1,38 @@
 package json;
 
 
+import org.apache.commons.io.FileUtils;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
 import org.everit.json.schema.loader.SchemaClient;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RequestValidatorImpl implements RequestValidator {
-    private static final String BT_REQUEST_SCHEMA = "BT_REQUEST_SCHEMA";
-    private static final String BT_JSON_SCHEMAS_CLASSPATH = "BT_JSON_SCHEMAS_CLASSPATH";
-    private static final Map<String, Schema> schemas = new HashMap<>();
+    private static Schema BT_REQUEST_SCHEMA;
     static {
-        init();
+        try {
+            BT_REQUEST_SCHEMA = SchemaLoader
+                    .builder()
+                    .resolutionScope(RequestValidatorImpl.class.getResource(
+                    "../json/schemas/").toURI()) // root schemas folder URI
+                    .schemaClient(SchemaClient.classPathAwareClient())
+                    .schemaJson(new JSONObject(FileUtils.readFileToString(new File(
+                        RequestValidatorImpl.class.getResource(
+                    "../json/schemas/BTRequest.json").toURI())))) // the main request schema by itself
+                    .build()
+                    .load()
+                    .build();
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
     @Override
     public void validate(JSONObject jsonObject) throws ValidationException {
-        schemas.get(BT_REQUEST_SCHEMA).validate(jsonObject);
-    }
-
-    private static void init() {
-        Schema requestSchema = SchemaLoader
-                .builder()
-                .resolutionScope(System.getProperties().getProperty(BT_JSON_SCHEMAS_CLASSPATH)) // root schemas folder URI
-                .schemaClient(SchemaClient.classPathAwareClient())
-                .schemaJson(new JSONObject(System.getProperties().getProperty(BT_REQUEST_SCHEMA))) // the schema by itself
-                .build()
-                .load()
-                .build();
-        schemas.put(BT_REQUEST_SCHEMA, requestSchema);
+        BT_REQUEST_SCHEMA.validate(jsonObject);
     }
 }
